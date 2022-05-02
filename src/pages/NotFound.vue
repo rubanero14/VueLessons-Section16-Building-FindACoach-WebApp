8ec0742998b3d6b72b4274b7945fc6525f60bc3c
Rajaruban Rajindram <ahyat888@gmail.com>
2022-05-02 18:40:17 +0800

    wip

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page is unavailable, go back to our <router-link to="/">Homepage</router-link>!</p>
+        </base-card>
+    </section>
+</template>
